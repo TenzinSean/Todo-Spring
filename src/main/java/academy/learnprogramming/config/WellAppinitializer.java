eec950a016d9d52eb34c06fcdb2ecc24e7c735d7
Tenzin <tenzin.lehpel@liip.ch>
2019-01-07 23:45:26 +0100

    update

---
+++ src/main/java/academy/learnprogramming/config/WellAppinitializer.java
@@ -0,0 +1,39 @@
+package academy.learnprogramming.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+@Slf4j
+public class WellAppinitializer implements WebApplicationInitializer {
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        log.info("onStartup");
+
+        // Create the spring application context
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext();
+        context.register(WebConfig.class);
+
+        // create the dispatcher servlet
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+
+        // register and configure the dispatcher servlet
+        ServletRegistration.Dynamic registration = servletContext.addServlet(DISPATCHER_SERVLET_NAME, dispatcherServlet);
+
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+
+
+
+    }
+}
